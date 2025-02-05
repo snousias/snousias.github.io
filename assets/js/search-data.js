73fd9b078b5d2227566e30f56b57d6c68e84b55f
snousias <nousias.stavros@gmail.com>
2025-02-05 22:03:02 +0000

    Deploying to gh-pages from @ snousias/snousias.github.io@ccc4d45ac0a8d547164b305fb4a269f8a862f5f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
         },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
